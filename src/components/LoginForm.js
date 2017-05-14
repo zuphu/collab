aa03c5b11307e8dbbf19646ef476ec22846df8c2
Ali Avci <aiavci@hotmail.com>
2017-05-14 01:06:11 -0400

    Login form placed in Paper component

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -2,26 +2,33 @@ import React, { Component } from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import RaisedButton from 'material-ui/RaisedButton';
 import TextField from 'material-ui/TextField';
+import Paper from 'material-ui/Paper';
 
 
 class LoginForm extends React.Component {
   constructor(props) {
     super(props);
   }
-
   render() {
+    const style = {
+      textAlign: 'center',
+      display: 'inline-block',
+      padding: '50px 50px 50px 50px'
+    };
     return (
-      <form>
-        <TextField
-          hintText="User name"
-        /><br />
-        <TextField
-          hintText="Password Field"
-          floatingLabelText="Password"
-          type="password"
-        /><br />
-        <RaisedButton label="Login" primary={true}/>
-      </form>
+      <Paper style={style} zDepth={1}>
+        <form>
+          <TextField
+            hintText="User name"
+          /><br />
+          <TextField
+            hintText="Password Field"
+            floatingLabelText="Password"
+            type="password"
+          /><br />
+          <RaisedButton label="Login" primary={true}/>
+        </form>
+      </Paper>
     );
   }
 }
