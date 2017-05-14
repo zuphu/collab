5e60af72e7033dbdec2bff19b11f7f97fa247d74
Ali Avci <aiavci@hotmail.com>
2017-05-14 00:48:45 -0400

    Folder created to store components

--- src/App.js
+++ src/App.js
@@ -2,40 +2,9 @@ import React, { Component } from 'react';
 import logo from './../public/favicon-circular.ico';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import RaisedButton from 'material-ui/RaisedButton';
+import Clock from './components/Clock';
+import LoginForm from './components/LoginForm';
 import './App.css';
-
-class Clock extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {date: new Date()};
-  }
-
-  componentDidMount() {
-    this.timerID = setInterval(
-      () => this.tick(),
-      1000
-    );
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.timerID);
-  }
-
-  tick() {
-    this.setState({
-      date: new Date()
-    });
-  }
-
-  render() {
-    return (
-      <div>
-        <h1>Welcome!</h1>
-        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
-      </div>
-    );
-  }
-}
 
 class App extends Component {
   render() {
@@ -46,6 +15,7 @@ class App extends Component {
             <img src={logo} className="App-logo" alt="logo" />
           </div>
           <Clock />
+          <LoginForm />
           <p className="App-intro">
             To get started, edit <code>src/App.js</code> and save to reload.
           </p>
---
+++ src/components/LoginForm.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import RaisedButton from 'material-ui/RaisedButton';
+
+class LoginForm extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        Login form here
+      </div>
+    );
+  }
+}
+
+export default LoginForm;
