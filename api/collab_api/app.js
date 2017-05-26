0f52ffe4800a2c69894728e6b692f705f3a66773
Anthony Guevara (scorpionix) <anthony.guev@gmail.com>
2017-05-25 23:44:37 -0400

    Add mongodb local connection.

--- api/collab_api/app.js
+++ api/collab_api/app.js
@@ -1,19 +1,25 @@
 var express = require('express');
 var path = require('path');
+var debug = require('debug')('collab-api:app');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
+var passport = require('passport');
+var flash = require('flash');
+var session = require('express-session');
+var configDB = require('./config/database.js');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+var index = require('./routes/index.js');
+var users = require('./routes/users.js');
 
 var app = express();
 
 // view engine setup
+app.use(express.static('views'));
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -22,6 +28,15 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+	secret: 'runningwithscissors'
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+
+debug(configDB);
+mongoose.connect(configDB.url); // connect to our database
 
 app.use('/', index);
 app.use('/users', users);
