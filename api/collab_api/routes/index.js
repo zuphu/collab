806ef65c18ddbfa561785dbcf2cc73d39452b3c3
Anthony Guevara (scorpionix) <anthony.guev@gmail.com>
2017-05-30 22:29:20 -0400

    Add index routes and some new libraries to package.json.

--- api/collab_api/routes/index.js
+++ api/collab_api/routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
+var debug = require('debug')('collab-api:index.js');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function(req, res) {
+  res.render('index');
+});
+
+router.get('/signup', function(req, res) {
+  res.render('signup');
+});
+
+router.get('/login', function(req, res) {
+  res.render('login');
 });
 
 module.exports = router;
