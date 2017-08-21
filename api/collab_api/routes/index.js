3ff2e1d352c5b32b2c944456ebe52a0387561a4f
Anthony Guevara (scorpionix) <anthony.guev@gmail.com>
2017-08-21 00:57:40 -0400

    Update gitignore.

--- api/collab_api/routes/index.js
+++ api/collab_api/routes/index.js
@@ -1,23 +1,9 @@
 var express = require('express');
 var router = express.Router();
-var path = require('path');
-var debug = require('debug')('collab-api:index.js');
-var passport = require('passport');
 
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index');
 });
-
-router.get('/signup', function(req, res) {
-  res.render('signup');
-});
-
-// process the signup form
-router.post('/signup', passport.authenticate('local-signup', {
-  successRedirect : '/profile', // redirect to the secure profile section
-  failureRedirect : '/signup', // redirect back to the signup page if there is an error
-  failureFlash : true // allow flash messages
-}));
 
 module.exports = router;
