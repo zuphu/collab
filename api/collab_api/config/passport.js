84aa953726b82c84fa218caeb6ede6e9c0af72dd
Anthony Guevara (scorpionix) <anthony.guev@gmail.com>
2017-08-31 00:02:15 -0400

    Add local-login passport authentication.

--- api/collab_api/config/passport.js
+++ api/collab_api/config/passport.js
@@ -4,7 +4,7 @@
 var LocalStrategy   = require('passport-local').Strategy;
 
 // load up the user model
-var User            = require('../models/user');
+var User            = require('../models/user.js');
 
 // expose this function to our app using module.exports
 module.exports = function(passport) {
@@ -49,12 +49,13 @@ module.exports = function(passport) {
         // we are checking to see if the user trying to login already exists
         User.findOne({ 'local.email' :  email }, function(err, user) {
             // if there are any errors, return the error
+debugger;
             if (err)
                 return done(err);
 
             // check to see if theres already a user with that email
             if (user) {
-                return done(null, false, req.flash('signupMessage', 'That email is already taken.'));
+                return done(null, false, {message: 'That email is already taken.'});
             } else {
 
                 // if there is no user with that email
@@ -79,5 +80,33 @@ module.exports = function(passport) {
 
     }));
 
-};
+    passport.use('local-login', new LocalStrategy({
+        // by default, local strategy uses username and password, we will override with email
+        usernameField : 'email',
+        passwordField : 'password',
+        passReqToCallback : true // allows us to pass back the entire request to the callback
+    },
+    function(req, email, password, done) { // callback with email and password from our form
+        // find a user whose email is the same as the forms email
+        // we are checking to see if the user trying to login already exists
+        User.findOne({ 'local.email' :  email }, function(err, user) {
+		debugger;
+            // if there are any errors, return the error before anything else
+            if (err)
+                return done(err);
 
+            // if no user is found, return the message
+            if (!user)
+                return done(null, false, {message: 'No user found.' }); // req.flash is the way to set flashdata using connect-flash
+
+            // if the user is found but the password is wrong
+           if (!user.validPassword(password))
+                return done(null, false, {message: 'Oops! Wrong password.'}); // create the loginMessage and save it to session as flashdata
+
+            // all is well, return successful user
+            return done(null, user);
+        });
+
+    }));
+
+};
