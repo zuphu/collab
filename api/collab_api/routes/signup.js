9ff2c8a790ccd00aad9970aa02394938132d41c8
Anthony Guevara (scorpionix) <anthony.guev@gmail.com>
2017-09-01 00:08:11 -0400

    Print correct signup error messages in signup.js

--- api/collab_api/routes/signup.js
+++ api/collab_api/routes/signup.js
@@ -3,16 +3,17 @@ var router = express.Router();
 var path = require('path');
 var debug = require('debug')('collab-api:index.js');
 var passport = require('passport');
+var flash = require('connect-flash');
 
 router.get('/', function(req, res) {
-  res.render('signup', { message: req.flash('signupMessage') });
+  res.render('signup', { message: req.flash('error') });
 });
 
 // process the signup form
 router.post('/', passport.authenticate('local-signup', {
   successRedirect : '/profile', // redirect to the secure profile section
   failureRedirect : '/signup', // redirect back to the signup page if there is an error
-  failureFlash : true // allow flash messages
+  failureFlash : true, // allow flash messages
 }));
 
 module.exports = router;
