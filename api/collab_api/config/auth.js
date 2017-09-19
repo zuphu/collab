8114139a3e21d4297dc9278140dbc0fe9b885946
Anthony Guevara (scorpionix) <anthony.guev@gmail.com>
2017-09-18 22:06:43 -0400

    Update auth params for facebook login.

--- api/collab_api/config/auth.js
+++ api/collab_api/config/auth.js
@@ -1,9 +1,7 @@
 module.exports = {
-/*
     'facebookAuth' : {
-        'clientID'      : '120329195354838', // your App ID
-        'clientSecret'  : 'supersecret', // your App Secret
-        'callbackURL'   : 'http://localhost:8080/auth/facebook/callback'
+        'clientID'      : '1568619736538826', // your App ID
+        'clientSecret'  : '1de64cb52ea71cba5a98fe3fa2107f5a', // your App Secret
+        'callbackURL'   : 'http://localhost:3000/auth/facebook/callback'
     }
-*/
 };
