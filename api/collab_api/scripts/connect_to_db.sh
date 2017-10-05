8bb95035e97e4d6c2fd0cf8ad5cc344cbd70dcff
Anthony Guevara (scorpionix) <anthony.guev@gmail.com>
2017-10-04 23:47:39 -0400

    Fix connect to mongodb script.

--- api/collab_api/scripts/connect_to_db.sh
+++ api/collab_api/scripts/connect_to_db.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
 FILTER="bitnami/mongodb"
-MONGNO_DB_CONTAINER=`docker ps | grep $FILTER | awk '{print $1}'`
-
-if [ -z "{$MONGO_DB_CONTAINER}" ]; then
+MONGO_DB_CONTAINER=`docker ps | grep $FILTER | awk '{print $1}'`
+if [ -z "${MONGO_DB_CONTAINER}" ]; then
 	echo $?
 	echo 'Unable to locate docker container'
 	exit 1
 fi
+
+echo ============
+echo "Connecting to container $MONGO_DB_CONTAINER"
+echo ============
 
 docker exec -it $MONGO_DB_CONTAINER /bin/bash
